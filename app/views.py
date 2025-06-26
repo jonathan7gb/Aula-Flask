d48e6e3d0dbe50a0d97137727546b561d4720c9f
jonathan_uber <jonathanuber121207@gmail.com>
2025-06-26 10:52:01 -0300

    cria a rota para área sobre nós

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/sobre/')
+def about():
+    return render_template('sobre.html')
