49a3b096641a6d414bc4ae5c5922d3e8f4b4a191
jonathan_uber <jonathanuber121207@gmail.com>
2025-07-02 08:38:13 -0300

    criando sqlalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 #Inicializa os pacotes
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app.views import homepage
