b6a216d9a8f6c7e2a4092fcca8376cf547b2856d
jonathan_uber <jonathanuber121207@gmail.com>
2025-08-08 07:21:20 -0300

    add secret key

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'jdasjn1312sdasajnas'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
