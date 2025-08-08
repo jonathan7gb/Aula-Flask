0256ef793f1d65098a2b40e0758ac00c627c8759
jonathan_uber <jonathanuber121207@gmail.com>
2025-08-08 08:05:21 -0300

    adiciona classe user

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
 import os
 load_dotenv('.env')
 
@@ -13,9 +16,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'jdasjn1312sdasajnas'
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
 
 from app.views import homepage
 from app.models import Contato
--- app/models.py
+++ app/models.py
@@ -1,5 +1,18 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=True)
+    sobrenome = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(100), nullable=True)
+    senha = db.Column(db.String(100), nullable=True)
+
 
 class Contato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
