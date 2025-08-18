0aa285e5fb8b021e9eac727867defd7f8dc9847e
jonathan_uber <jonathanuber121207@gmail.com>
2025-08-18 08:00:37 -0300

    a

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for, request, redirect
 from app.forms import contatoForm, userForm
 from app.models import Contato, User
 from app import bcrypt
+from flask_login import login_user, logout_user, current_user
 
 @app.route('/')
 def homepage():
@@ -49,6 +50,7 @@ def cadastro_usuario():
     form = userForm()
     context = {}
     if form.validate_on_submit():
-        form.save()
+        user = form.save()
+        login_user(user, remember=True)
         return redirect(url_for('homepage'))
     return render_template('cadastro_user.html', context=context, form=form)
